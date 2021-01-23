3823fd724adf7a37fabddc83b0d54d794ded3353
M-Shikhar <77896163+M-Shikhar@users.noreply.github.com>
2021-01-24 00:27:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ print("------------------------------------------------")
 print("What do you want to do?")
 print("1.Addition")
 print("2.Subtraction")
-a = int(input("Pick 1 or 2\n"))
+print("3.Multiplication")
+print("4.Division")
+a = int(input("Pick 1, 2, 3 or 4\n"))
 if a == 1:
     b = int(input("Enter the First Number\n"))
     c = int(input("Enter the Second Number\n"))
@@ -15,19 +17,31 @@ if a == 2:
     e = int(input("Enter the Second Number\n"))
     f = d-e
     print("The Result is",f,"!")
-if a != 1 and a != 2:
+if a == 3:
+    h = int(input("Enter the First Number\n"))
+    i = int(input("Enter the second Number\n"))
+    j = h*i
+    print("The Result is",j,"!")
+if a == 4:
+    k = int(input("Enter the First Number\n"))
+    l = int(input("Enter the Second Number\n"))
+    m = k/l
+    print("The Result is",m,"!")
+if a != 1 and a != 2 and a != 3 and a!= 4:
     print("That's not a valid option")
 g = input("Do you want to end or continue?\n")
-if g == "end":
-    print("Goodbye!")
 while g != "end" and g != "continue":
     print("That's not a valid option")
     g = input("Do you want to end or continue?\n")
+if g == "end":
+    print("Goodbye!")
 while g == "continue":
     print("What do you want to do?")
     print("1.Addition")
     print("2.Subtraction")
-    a = int(input("Pick 1 or 2\n"))
+    print("3.Multiplication")
+    print("4.Division")
+    a = int(input("Pick 1, 2, 3 or 4\n"))
     if a == 1:
         b = int(input("Enter the First Number\n"))
         c = int(input("Enter the Second Number\n"))
@@ -38,11 +52,21 @@ while g == "continue":
         e = int(input("Enter the Second Number\n"))
         f = d-e
         print("The Result is",f,"!")
-    if a != 1 and a != 2:
+    if a == 3:
+        h = int(input("Enter the First Number\n"))
+        i = int(input("Enter the second Number\n"))
+        j = h*i
+        print("The Result is",j,"!")
+    if a == 4:
+        k = int(input("Enter the First Number\n"))
+        l = int(input("Enter the Second Number\n"))
+        m = k/l
+        print("The Result is",m,"!")
+    if a != 1 and a != 2 and a != 3 and a != 4:
         print("Thats not a valid option")
     g = input("Do you want to end or continue?\n")
-    if g == "end":
-        print("Goodbye!")
     while g != "continue" and g != "end":
         print("That's not a valid option\n")
         g = input("Do you want to end or continue?\n")
+    if g == "end":
+        print("Goodbye!")
